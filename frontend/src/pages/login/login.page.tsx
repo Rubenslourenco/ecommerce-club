cfcdd6c2d8c19ff500a32129e1ff71f9443f50aa
Rubenslourenco <alvesrubens926@gmail.com>
2026-01-27 22:53:55 -0300

    feat: add Custom Button to LoginPage

--- frontend/src/pages/login/login.page.tsx
+++ frontend/src/pages/login/login.page.tsx
@@ -1,3 +1,6 @@
+// import { BsGoogle } from "react-icons/bs";
+
+import CustomButton from "../../components/custom-buttom/custom-button.component";
 import Header from "../../components/header/header.component";
 import {
   LoginContainer,
@@ -16,10 +19,22 @@ const LoginPage = () => {
         <LoginContent>
           <LoginHeadline>Entre com a sua conta</LoginHeadline>
 
+          <CustomButton
+          // startIcon={
+          //   <span>
+          //     <BsGoogle size={25} />
+          //   </span>
+          // }
+          >
+            Entrar com Google
+          </CustomButton>
+
           <LoginSubtitle>ou entre com seu email</LoginSubtitle>
 
           <LoginInputContainer></LoginInputContainer>
           <LoginInputContainer></LoginInputContainer>
+
+          <CustomButton>Entrar</CustomButton>
         </LoginContent>
       </LoginContainer>
     </>
