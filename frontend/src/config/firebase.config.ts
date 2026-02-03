f831bde96bc4eddf3882cf78fab37a8c4f9bf73a
Rubenslourenco <alvesrubens926@gmail.com>
2026-02-02 23:11:08 -0300

    core: initiliza firebase auth

--- frontend/src/config/firebase.config.ts
+++ frontend/src/config/firebase.config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBi2A6HHMEFZCW5jPvs5eMPc73OC7P_uMw",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
