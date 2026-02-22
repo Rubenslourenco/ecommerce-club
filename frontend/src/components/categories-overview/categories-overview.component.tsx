a7a2730d673081c99a89492461074561f13f8792
Rubenslourenco <alvesrubens926@gmail.com>
2026-02-21 23:13:33 -0300

    feat: add Loading to Categories Overview

--- frontend/src/components/categories-overview/categories-overview.component.tsx
+++ frontend/src/components/categories-overview/categories-overview.component.tsx
@@ -5,15 +5,20 @@ import { Container } from "./categories-overview.styles";
 import { CategoryContext } from "../../contexts/category.context";
 
 import CategoryOverview from "../category-overview/category-overview.component";
+import Loading from "../loading/loading.component";
 
 const CategoriesOverview: FunctionComponent = () => {
-  const { categories, fetchCategories } = useContext(CategoryContext);
+  const { categories, isLoading, fetchCategories } =
+    useContext(CategoryContext);
 
   useEffect(() => {
     if (categories.length === 0) {
       fetchCategories();
     }
   }, []);
+
+  if (isLoading) return <Loading />;
+
   return (
     <Container>
       {categories.map((category) => (
